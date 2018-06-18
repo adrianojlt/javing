ffed8252312f3bf45d2b330c80a2fee82b7a3011
Adriano Teixeira <adrianojlt@gmail.com>
2018-06-18 18:04:47 +0100

    tmp pairs exerc example

--- src/pt/adrz/myjavatutorial/tmp/Tmp.java
+++ src/pt/adrz/myjavatutorial/tmp/Tmp.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,9 +17,64 @@ public class Tmp {
 		
 		//example();
 		//tmp();
-		increment();
+		//increment();
+		//invert();
+		testPairs();
 		//teste();
 		//input();
+		
+		
+	}
+	
+	public static void testPairs() {
+		int arr[] = {1,5,3,4,2};
+		
+		int res = pairs(2, arr);
+		System.out.println(res);
+	}
+	
+	//static 
+	public static int pairs(int k, int[] arr) {
+		
+		HashMap<Integer, Integer> hm = new HashMap<Integer,Integer>();
+		
+		//hm.containsKey(arg0)
+		//hm.put(2, 3);
+		int total = 0;
+		
+		for (int i = 0 ; i < arr.length ; i++ ) {
+			for (int z = i + 1 ; z < arr.length ; z++ ) {
+				
+				int diff = Math.abs(arr[i] - arr[z]);
+				
+				if ( diff == k ) {
+					total++;
+				}
+				
+				//if ( hm.containsKey(diff) ) {
+					//hm.put(diff, hm.get(diff) + 1);
+				//}
+				//arr[i] - arr[z];
+			}
+		}
+		
+		//System.out.println(Arrays.asList(hm)); 
+		
+		/*
+		for (int i : arr) {
+			
+		}*/
+		
+		return total;
+	}
+	
+	public static void invert() {
+		Integer[] arr = {1,4,3,2};
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	public static void streams() {
+		List<Integer> numeros = Arrays.asList(new Integer(10),new Integer(3));
 	}
 	
 	public static void increment() {
